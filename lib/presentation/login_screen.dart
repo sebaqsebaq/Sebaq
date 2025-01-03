78f517d25f1416e65102a33b1eba5e644de7ab06
ahmad <ahmadabogali29@gmail.com>
2025-01-03 23:16:26 +0300

    Add New Page(login+splash screen)

--- lib/presentation/login_screen.dart
+++ lib/presentation/login_screen.dart
@@ -1,6 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:untitled1/presentation/login_screen.dart';
 import 'package:untitled1/presentation/splash_screen.dart';
+
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login'),
+      ),
+      body: Center(
+        child: Text('Welcome to the Login Screen!'),
+      ),
+    );
+  }
+}
 
 void main() {
   runApp(MyApp());
